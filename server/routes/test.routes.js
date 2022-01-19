aaaf4fb7b05e0783426e4e1a10526f9e5b515393
akshaykanade <akshayk0782@gmail.com>
2022-01-20 03:26:11 +0530

    :truck: add: new routes

--- server/routes/test.routes.js
+++ server/routes/test.routes.js
@@ -1,21 +1,47 @@
-const express = require('express');
-const { createTest, userCreatedTests, testAdminData } = require('../controllers/test.control');
-const { requireSignIn } = require('../middlewares');
+const express = require("express");
+const {
+    createTest,
+    userCreatedTests,
+    testAdminData,
+    testRegister,
+    increasePersonDetected,
+    increaseVoiceDetected,
+    increaseFaceCovering,
+    totalWarnings,
+    terminateExam,
+    allowInExam,
+} = require("../controllers/test.control");
+const { requireSignIn } = require("../middlewares");
 const router = express.Router();
 
 // user/admin can create test
-router.post('/create-test', requireSignIn, createTest);
+router.post("/create-test", requireSignIn, createTest);
 
 // user/admin can fetch a list of test which we created
-router.get('/all-created-test', requireSignIn, userCreatedTests);
+router.get("/all-created-test", requireSignIn, userCreatedTests);
+
+// for registering the exam
+router.patch("/test-register/:test_code", requireSignIn, testRegister);
 
 // user/admin can fetch the live status of test
-router.get('/test-live-status/:testId', requireSignIn, testAdminData);
+router.get("/test-live-status/:test_code", requireSignIn, testAdminData);
 
-// for joining the test 
-router.get('/test/:testId',) // working on middlewares
+// increasing warning count of person detected
+router.patch("/warning-person-detected", requireSignIn, increasePersonDetected);
 
-// remaining 
-router.patch('/register')
+// increasing warning count of voice-detected
+router.patch("/warning-voice-detected", requireSignIn, increaseVoiceDetected);
+
+// increasing warning count of face-covering
+router.patch("/warning-face-covering", requireSignIn, increaseFaceCovering);
+
+// will give you total no of warning given to user
+router.get("/total-warnings", requireSignIn, totalWarnings);
+
+// route that will terminate the exam of candidate
+router.patch("/terminate", requireSignIn, terminateExam);
+
+// route that will allow the terminated candidates to give an exam
+router.patch("/allow-in-exam", requireSignIn, allowInExam);
 
 module.exports = router;
