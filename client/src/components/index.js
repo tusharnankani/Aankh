4e08d1b3ac24ba7fbb99993c7e7c743a8cf17547
tusharnankani <tusharnankani3@gmail.com>
2022-01-20 02:59:02 +0530

    :sparkles: feat: create separate livecapture component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,6 +2,7 @@ export { default as Navbar } from './navbar/Navbar';
 export { default as CtaButton } from './ctabutton/CtaButton';
 export { default as CommonInput } from './commoninput/CommonInput';
 export { default as WebcamCapture } from './webcamcapture/WebcamCapture';
+export { default as WebLiveCapture } from './weblivecapture/WebLiveCapture';
 export { default as CopyLink } from './copylink/CopyLink';
 export { default as Terminated } from './terminated/Terminated';
 export { default as Timer } from './timer/Timer';
---
+++ client/src/components/weblivecapture/WebLiveCapture.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import Webcam from 'react-webcam';
+import './weblivecapture.css';
+
+const videoConstraints = {
+	width: 1280,
+	height: 720,
+	facingMode: 'user'
+};
+
+const WebLiveCapture = () => {
+	const webcamRef = React.useRef(null);
+	const [ image, setImage ] = useState('');
+	const capture = React.useCallback(
+		() => {
+			const imageSrc = webcamRef.current.getScreenshot();
+			setImage(imageSrc);
+			console.log('Captured');
+		},
+		[ webcamRef ]
+	);
+
+	return (
+		<React.Fragment>
+			<Webcam
+				audio={false}
+				ref={webcamRef}
+				screenshotFormat="image/jpeg"
+				height={150}
+				width={300}
+				videoConstraints={videoConstraints}
+			/>
+
+			<button onClick={capture}>Capture photo</button>
+		</React.Fragment>
+	);
+};
+
+export default WebLiveCapture;
