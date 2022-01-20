de87d48f948146a610fe628bf14f49099fd1ee7c
viveknamaye <namayevivek87@gmail.com>
2022-01-20 09:34:43 +0530

    :sparkles: add model_api_service

--- model/app.py
+++ model/app.py
@@ -1,10 +1,13 @@
+import base64
+import matplotlib.pyplot as plt
 from flask import Flask
 
+import os
 from flask import request, jsonify
 import tensorflow as tf 
 import tensorflow_hub as hub 
 import cv2 
-import numpy 
+import numpy as np
 from mark_detector import MarkDetector
 from pose_estimator import PoseEstimator
 
@@ -14,35 +17,52 @@ multiple_people_detector = hub.load("https://tfhub.dev/tensorflow/efficientdet/d
 
 
 
-@app.route('/predict_people',methods=['GET','POST'])
-def predict_pose():
-    data = request.get_json()
+# @app.route('/predict_people',methods=['GET','POST'])
+# def predict_pose():
+#     data = request.get_json()
+#     # get image tensor 
+#     output = multiple_people_detector(image, threshold = 0.5)
+#     people = 0
+#     for i in range(int(output['num_detections'][0])):
+#         if classes[i] == 1 and scores[i] > threshold:
+#             people += 1
+#             ymin, xmin, ymax, xmax = boxes[i]
+#             (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
+#                                           ymin * im_height, ymax * im_height)
+#             draw.line([(left, top), (left, bottom), (right, bottom), (right, top), (left, top)],
+#                       width=4, fill='red')
 
-    # get image tensor 
-    output = multiple_people_detector(image, threshold = 0.5)
-    people = 0
-    for i in range(int(output['num_detections'][0])):
-        if classes[i] == 1 and scores[i] > threshold:
-            people += 1
-            ymin, xmin, ymax, xmax = boxes[i]
-            (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
-                                          ymin * im_height, ymax * im_height)
-            draw.line([(left, top), (left, bottom), (right, bottom), (right, top), (left, top)],
-                      width=4, fill='red')
+#     return jsonify({ 'people' : int(people) , 'image' : image})
 
-    return jsonify({ 'people' : int(people) , 'image' : image})
+
+
+
+
+
+def readb64(uri):
+   encoded_data = uri.split(',')[1]
+   nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
+   img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+   img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+   return img
+
 
 @app.route('/predict_pose', methods = ['GET', 'POST']) 
 def predict_pose() : 
-    data request.json() 
-
-    # get image tensor
-    height , width = img.shape[0], img.shape[1]
+    data = request.get_json(force = True) 
+    image = r'{}'.format(data['img'])
+    print(type(image), image)
+    image= readb64(image)
+    plt.imshow(image)
+    # plt.show()
+    # plt.imsave(image, 'sample.jpg');
+    height, width = image.shape[0], image.shape[1]
     pose_estimator = PoseEstimator(img_size=(height, width))
     mark_detector = MarkDetector()
 
-    facebox = mark_detector.extract_cnn_facebox(iamge)
+    facebox = mark_detector.extract_cnn_facebox(image)
         # Any face found?
+    frame = image
     if facebox is not None:
 
         # Step 2: Detect landmarks. Crop and feed the face area into the
@@ -75,7 +95,53 @@ def predict_pose() :
 
         # Do you want to see the facebox?
         # mark_detector.draw_box(frame, [facebox])
-        return jsonify({'img' : img, 'pose' : pose})
+        img = list(img)
+        return jsonify({'img' : 'face found', 'pose' : pose})
+    else :
+        return jsonify({'message' : 'face not found', 'img' : 'img'})
+
+
+
+
+@app.route('/predict_people',methods=['GET','POST'])
+def predict() : 
+    data = request.get_json(force = True)
+    image= readb64(data['img'])
+    im_width, im_height = image.shape[0], image.shape[1]
+    image = image.reshape((1, image.shape[0], image.shape[1], 3))
+    # return jsonify({'data' : data})
+    data = multiple_people_detector(image)
+
+    boxes = data['detection_boxes'].numpy()[0]
+    classes = data['detection_classes'].numpy()[0]
+    scores = data['detection_scores'].numpy()[0]
+
+    threshold = 0.5
+    people = 0
+    for i in range(int(data['num_detections'][0])):
+        if classes[i] == 1 and scores[i] > threshold:
+            people += 1
+            ymin, xmin, ymax, xmax = boxes[i]
+            (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
+                                          ymin * im_height, ymax * im_height)
+            # draw.line([(left, top), (left, bottom), (right, bottom), (right, top), (left, top)],
+            #           width=4, fill='red')
+
+    return jsonify({ 'people' : int(people) , 'image' : 'image'})
+
+
+@app.route('/save_img', methods=['GET', 'POST']) 
+def save() : 
+    data = request.get_json(force = True)
+    image = r'{}'.format(data['img'])
+    user = data['user']
+    image= readb64(image)
+    base_dir = os.getcwd()
+    path = r"{}\images\{}.jpg".format(base_dir, user[0:-10])
+    print(path)
+    plt.imsave(image, path)
+    return jsonify({'path' : path})
+
 
 if __name__ == '__main__':
     # app.run(debug=True)
