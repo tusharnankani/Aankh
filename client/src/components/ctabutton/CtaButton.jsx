f270b9486e370e0d0f0f35dfba7a03be17c90b4e
Akshay Kanade <68458601+kanadeakshay@users.noreply.github.com>
2024-12-18 19:27:38 +0530

    Initialize fresh branch for Nx mono-repo setup

--- client/src/components/ctabutton/CtaButton.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './ctabutton.css';
-
-const CtaButton = ({ text = 'Get Started' }) => {
-	return <button className="ctabutton">{text}</button>;
-};
-
-export default CtaButton;
